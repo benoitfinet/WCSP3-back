45ce3eff05959b7ec00b2aafe4f74c1ee94e9869
helder-hanka <hfernandes238@gmail.com>
2020-12-16 20:02:55 +0100

    add recover data and with id

---
+++ Model/photo.js
@@ -0,0 +1,16 @@
+const mysql = require('../db');
+
+const findAllPhoto = async () => {
+  const result = await mysql.query('SELECT * FROM photo');
+  return result[0];
+};
+
+const findPhoto = async (id) => {
+  const result = await mysql.query('SELECT * FROM photo WHERE id = ?', id);
+  return result[0];
+};
+
+module.exports = {
+  findAllPhoto,
+  findPhoto
+};
