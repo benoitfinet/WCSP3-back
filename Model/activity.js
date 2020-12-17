3746457bad746c333b86bd1b1bfba79fc986f083
helder-hanka <hfernandes238@gmail.com>
2020-12-17 18:10:09 +0100

    add indOneActivityPhoto, make query table of id activity

--- Model/activity.js
+++ Model/activity.js
@@ -1,28 +1,22 @@
 const mysql = require('../db');
 
 const findActivityPhotoAll = async () => {
-  const result = await mysql.query('SELECT * FROM photo as photo JOIN activity as activity');
+  const result = await mysql.query(
+    'SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id');
+  // const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = 1 JOIN photo AS ph ON ap.photo_id = ph.id');
+  // const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id ');
+  return result[0];
+};
+
+const findOneActivityPhoto = async (id) => {
+  const result = await mysql.query(
+    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.id= ?', id);
+  // const result = await mysql.query('SELECT * FROM activity JOIN photo WHERE id = ?', id);
   console.log(result[0]);
   return result[0];
 };
 
 module.exports = {
-  findActivityPhotoAll
+  findActivityPhotoAll,
+  findOneActivityPhoto
 };
-
-// const mysql = require('../db');
-
-// const findAllPhoto = async () => {
-//   const result = await mysql.query('SELECT * FROM photo');
-//   return result[0];
-// };
-
-// const findPhoto = async (id) => {
-//   const result = await mysql.query('SELECT * FROM photo WHERE id = ?', id);
-//   return result[0];
-// };
-
-// module.exports = {
-//   findAllPhoto,
-//   findPhoto
-// };
