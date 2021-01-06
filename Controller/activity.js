c3f3f64f10812110611cc0407b716337333ea8c5
helder-hanka <hfernandes238@gmail.com>
2021-01-06 15:47:07 +0100

    Update getActivityPhotoAll for add a filter

--- Controller/activity.js
+++ Controller/activity.js
@@ -5,8 +5,16 @@ const {
 
 const getActivityPhotoAll = async (req, res) => {
   try {
-    const data = await findActivityPhotoAll();
+    // let sql = 'SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id';
+    // const sqlValues = [];
+
+    // if(req.query.name) {
+
+    // }
+    console.log(req.query.name);
+    const data = await findActivityPhotoAll(req.query.name);
     res.status(200).json(data);
+    console.log('data', data);
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Internal server error');
