287f687bf76e708572938a67360b2541aceed460
helder-hanka <hfernandes238@gmail.com>
2021-01-07 11:01:32 +0100

    Make the connection with db of findOneActivityPhotoByName and Update findOneActivityPhotoById

--- Model/activity.js
+++ Model/activity.js
@@ -1,32 +1,30 @@
 const mysql = require('../db');
 
-const findActivityPhotoAll = async (sqlValues) => {
-  // const result = await mysql.query(
-  //   'SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id'
-  // );
-  // return result[0];
-
-  // let sql = 'SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id';
-  // const sqlValues = [];
-
-  // sqlValues.push()
-
-  const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id', sqlValues);
+const findActivityPhotoAll = async () => {
+  const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id');
   return result[0];
-
-  // const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = 1 JOIN photo AS ph ON ap.photo_id = ph.id');
-  // const result = await mysql.query('SELECT * FROM activity AS ac JOIN activity_photo AS ap ON ap.activity_id = ac.id ');
 };
 
-const findOneActivityPhoto = async (id) => {
+const findOneActivityPhotoByName = async (name) => {
   const result = await mysql.query(
-    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.id= ?', id);
-  // const result = await mysql.query('SELECT * FROM activity JOIN photo WHERE id = ?', id);
+    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.name= ?', name);
   console.log(result[0]);
   return result[0];
 };
+const findOneActivityPhotoById = async (id) => {
+  const result = await mysql.query(
+    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.id= ?', id);
+  return result[0];
+};
+
+// const createOneActivity = async () = {
+//   const result = await mysql.query(
+//     'INSERT INTO activity SET '
+//   );
+// }
 
 module.exports = {
   findActivityPhotoAll,
-  findOneActivityPhoto
+  findOneActivityPhotoByName,
+  findOneActivityPhotoById
 };
