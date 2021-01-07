3d1a53fa7f1b754436a4fee24ee96d05e3a653bb
helder-hanka <hfernandes238@gmail.com>
2021-01-07 11:03:11 +0100

    Add a route for getOneActivityPhoto and Update getOneActivityPhotoById

--- routes/activity.js
+++ routes/activity.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const activityController = require('../Controller/activity');
 
 router.get('/', activityController.getActivityPhotoAll);
-router.get('/:id', activityController.getOneActivityPhoto);
+router.get('/name/:name', activityController.getOneActivityPhoto);
+router.get('/id/:id', activityController.getOneActivityPhotoById);
 
 module.exports = router;
