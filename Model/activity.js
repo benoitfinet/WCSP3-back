095fed8caa15b82315f13f287bcf18df0b9f11fe
helder-hanka <hfernandes238@gmail.com>
2021-01-19 09:44:41 +0100

    remove findOneActivityById in model

--- Model/activity.js
+++ Model/activity.js
@@ -1,24 +1,19 @@
 const mysql = require('../db');
 
 const findActivityPhotoAll = async () => {
-  const result = await mysql.query('SELECT * FROM activity AS ac JOIN price AS p ON p.activity_id = ac.id JOIN activity_photo AS ap ON ap.activity_id = ac.id JOIN photo AS ph ON ap.photo_id = ph.id');
+  const result = await mysql.query('SELECT * FROM activity ac JOIN price p ON p.activity_id = ac.id JOIN activity_photo ap ON ap.activity_id = ac.id JOIN photo ph ON ap.photo_id = ph.id JOIN price prc ON prc.activity_id = ac.id');
   return result[0];
 };
 
 const findOneActivityPhotoByName = async (name) => {
   const result = await mysql.query(
-    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id JOIN price AS p ON p.activity_id = ac.id WHERE ac.name= ?', name);
+    'SELECT * FROM activity ac JOIN price p ON p.activity_id = ac.id JOIN activity_photo ap ON ap.activity_id = ac.id JOIN photo ph ON ap.photo_id = ph.id JOIN price prc ON prc.activity_id = ac.id WHERE ac.name= ?', name);
   console.log(result[0]);
   return result[0];
 };
 const findOneActivityPhotoById = async (id) => {
   const result = await mysql.query(
-    'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id JOIN price AS p ON p.activity_id = ac.id WHERE ac.id= ?', id);
-  return result[0];
-};
-
-const findOneActivityById = async (id) => {
-  const result = await mysql.query('SELECT * FROM activity WHERE id = ?', id);
+    'SELECT * FROM activity ac JOIN price p ON p.activity_id = ac.id JOIN activity_photo ap ON ap.activity_id = ac.id JOIN photo ph ON ap.photo_id = ph.id JOIN price prc ON prc.activity_id = ac.id WHERE ac.id=?', id);
   return result[0];
 };
 
@@ -44,6 +39,5 @@ module.exports = {
   findActivityPhotoAll,
   findOneActivityPhotoByName,
   findOneActivityPhotoById,
-  createOneActivity,
-  findOneActivityById
+  createOneActivity
 };
