8b3f8cf71133c5c308fa1cf9ddd97d54c08418fb
helder-hanka <hfernandes238@gmail.com>
2021-01-21 07:32:09 +0100

    Add controller insert request

--- Controller/event.js
+++ Controller/event.js
@@ -1,103 +1,85 @@
 const {
-    findOnePhotoEventById,
-    findAllEventPhoto,
-    findOneEventPhotoByName,
-    createEvent,
-    findOneEventById,
-    createOneEventWithPhoto,
-    SelectToDeleteOneEvent
+  findOnePhotoEventById,
+  findAllEventPhoto,
+  findOneEventPhotoByName,
+  createOneEventWithPhoto,
+  SelectToDeleteOneEvent
 } = require('../Model/event');
 
-const getEventById = async (req, res) => {
-    try {
-        const data = await findOneEventById(req.params.id);
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(404).send('Event not found')
-    }
-};
-
-const postEvent = async (req,res) => {
-    try {
-        const data = await createEvent(req.body);
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('Internal server error');
-    }
-};
-
 const getOnePhotoEventById = async (req, res) => {
-    try{
-        const data = await findOnePhotoEventById(req.params.id);
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(404).send('Error photo not found')
+  try {
+    const data = await findOnePhotoEventById(req.params.id);
+    if (data.length > 0) {
+      return res.status(200).json(data);
     }
+    res.status(404).send('Error event not found');
+  } catch (error) {
+    console.error(error.message);
+    res.status(404).send('Error photo not found');
+  }
 };
 
 const getOneEventPhotoByName = async (req, res) => {
-    try {
-        const data = await findOneEventPhotoByName(req.query.name);
-        res.status(200).json(data);
-    } catch (error) {
-        console.error(error.message);
-        res.status(404).send('Error photo not found')
+  try {
+    const data = await findOneEventPhotoByName(req.params.name);
+    if (data.length > 0) {
+      return res.status(200).json(data);
     }
+    res.status(404).send('Name not found');
+  } catch (error) {
+    console.error(error.message);
+    res.status(404).send('Error photo not found');
+  }
 };
 
 const getAllEventPhoto = async (req, res) => {
-    try {
-        const data = await findAllEventPhoto();
-        res.status(200).json(data);
-    } catch (err) {
-        console.error(err.message);
-        res.status(404).send('Error photo not found')
-    }
+  try {
+    const data = await findAllEventPhoto();
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err.message);
+    res.status(404).send('Error photo not found');
+  }
 };
 
 const postOneEventWithPhoto = async (req, res) => {
-    try {
-        const eventBody = {
-            name : req.body.name,
-            description : req.body.description,
-            price : req.body.price,
-            age : req.body.age
-        };
-        const photoBody = {
-            title : req.body.title,
-            location : req.body.location
-        };
-        const data = await createOneEventWithPhoto(eventBody, photoBody);
-        console.log('data', data);
-        const newData = await findOneEventById(data[0].req.params.id);
-        console.log('new data', newData);
-        newData[0].photoID = data[0].eventID;
-        res.status(200).json(newData);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    const eventBody = {
+      name: req.body.name,
+      description: req.body.description,
+      price: req.body.price,
+      age: req.body.age
+    };
+    const photoBody = {
+      title: req.body.title,
+      location: req.body.location
+    };
+    const data = await createOneEventWithPhoto(eventBody, photoBody);
+    console.log('CONTROLLER data: ', data);
+    const newData = await findOnePhotoEventById(data.event_id);
+    console.log('CONTROLLER new data: ', newData);
+
+    res.status(200).json(newData[0]);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
 const deleteOneEvent = async (req, res) => {
-    try {
-        await SelectToDeleteOneEvent(req.params.id);
-        res.status(204).send('Event has been deleted');
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Internal Server Error');
-    }
+  try {
+    await SelectToDeleteOneEvent(req.params.id);
+    res.status(204).send('Event has been deleted');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+  }
 };
 
 module.exports = {
-    getOnePhotoEventById,
-    getAllEventPhoto,
-    getOneEventPhotoByName,
-    postEvent,
-    getEventById,
-    postOneEventWithPhoto,
-    deleteOneEvent
-    
+  getOnePhotoEventById,
+  getAllEventPhoto,
+  getOneEventPhotoByName,
+  postOneEventWithPhoto,
+  deleteOneEvent
 };
