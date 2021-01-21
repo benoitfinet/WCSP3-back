463cd2715ae82747805904163f7327c9ff9841cd
helder-hanka <hfernandes238@gmail.com>
2021-01-21 10:31:54 +0100

    delete comments

--- Model/activity.js
+++ Model/activity.js
@@ -6,21 +6,13 @@ const findActivityPhotoAll = async () => {
 };
 
 const findOneActivityPhotoByName = async (name) => {
-  const result = await mysql.query(
-  'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.name= ?', name);
+  const result = await mysql.query('SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.name= ?', name);
   return result[0];
 };
 const findOneActivityPhotoById = async (id) => {
-  const result = await mysql.query(
-  'SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.id= ?', id);
+  const result = await mysql.query('SELECT * FROM photo AS ph JOIN activity_photo AS ap ON ap.activity_id = ph.id JOIN activity AS ac ON ap.activity_id = ac.id WHERE ac.id= ?', id);
   return result[0];
 };
-
-// const createOneActivity = async () = {
-//   const result = await mysql.query(
-//     'INSERT INTO activity SET '
-//   );
-// }
 
 module.exports = {
   findActivityPhotoAll,
